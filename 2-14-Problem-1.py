4feed27cbf4ca61667baa628b12631cf9a2aff10
tim-clothier <trclothier@mavs.coloradomesa.edu>
2026-08-31 13:41:39 -0600

    Add files via upload

---
+++ 2-14-Problem-1.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        2.14 Problem 1
+# Purpose:
+#
+# Author:      trclothier
+#
+# Created:     31/08/2026
+# Copyright:   (c) trclothier 2026
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+ALL = "All"
+work = "work"
+AND = "and"
+no = "no"
+play = "play"
+makes = "makes"
+jack = "Jack"
+a = "a"
+dull = "dull"
+boy = "boy"
+
+print (ALL + ' ' + work + ' ' + AND + ' ' + no + ' ' + play + ' ' + makes + ' ' + jack + ' ' + a + ' ' + dull + ' ' + boy + '.')
+
