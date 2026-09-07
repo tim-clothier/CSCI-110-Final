32ee767c112494e975d6044bd26fde45a49570af
tim-clothier <trclothier@mavs.coloradomesa.edu>
2026-09-07 13:38:26 -0600

    Code cleanup on 3.8.11

--- Chapter-3.8.11.py
+++ Chapter-3.8.11.py
@@ -4,15 +4,6 @@ joey = turtle.Turtle()
 joey.color("black")
 joey.speed(1)
 
-
-#for i in range(5):
-    #Draw inner pentagon line first
-    #for j in range(3):
-        #joey.forward(100)
-        #joey.left(120)
-    #joey.forward(100)
-    #joey.right(72)
-
 for i in range(5):
     joey.forward(200)
     joey.right(144)
